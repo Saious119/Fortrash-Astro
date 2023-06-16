90cb53aeacf8c16c2c76c4b0818698590b6178ba
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2023-06-16 15:08:05 -0400

    Update Footer.tsx make here more noticeable

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -4,7 +4,7 @@ import { AppConfig } from '@/utils/AppConfig';
 
 const Footer = () => (
   <Section>
-    <>Check out the source code of this website <a href="https://github.com/Saious119/Fortrash-Astro">here!</a></>
+    <>Check out the source code of this website by clicking <b><a href="https://github.com/Saious119/Fortrash-Astro">here!</a></b></>
     <FooterCopyright site_name={AppConfig.site_name} />
   </Section>
 );
