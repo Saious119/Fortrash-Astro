0b148a138cb67a422653a5e872e87863d7bd73cd
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2023-06-16 15:05:20 -0400

    Update Footer.tsx with link to site source code

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -4,6 +4,7 @@ import { AppConfig } from '@/utils/AppConfig';
 
 const Footer = () => (
   <Section>
+    <>Check out the source code of this website <a href="https://github.com/Saious119/Fortrash-Astro">here!</a></>
     <FooterCopyright site_name={AppConfig.site_name} />
   </Section>
 );
